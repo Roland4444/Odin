b15ec52191b5036f57b5ce0d24d50abd8ba8dcfd
Roland4444 <turbopollux@rambler.ru>
2021-02-25 17:59:22 +0400

    adding java sample

--- src/main/java/se/roland/JHOF.java
+++ src/main/java/se/roland/JHOF.java
@@ -1,24 +1,17 @@
 package se.roland;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
 public class JHOF {
-    public  String marinafunc(String foo, String  bar){
+    private String marinafunc(String foo, String  bar){
         return "Σ="+(Integer.valueOf(foo)+Integer.valueOf(bar));
     };
-
-    public  String guest(String a, String b){
+    private String guest(String a, String b){
         return "";
     }
-
     public static String result(String foo, String bar, String name, Object obj) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method method =obj.getClass().getMethod(name, String.class, String.class);
-        method.setAccessible(true);
-        var result =   method.invoke(obj, foo, bar);
-        return "<h1>"+foo+"</h1><br>h1>"+bar+"</h1><br>"+result;
+        Method method =obj.getClass().getDeclaredMethod(name, String.class, String.class);
+        return "<h1>"+foo+"</h1><br>h1>"+bar+"</h1><br>"+method.invoke(obj, foo, bar);
     }
-
     public static String callmarina() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         var foo = "3";
         var bar = "7";
