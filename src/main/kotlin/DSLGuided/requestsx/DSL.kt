8617ae0ed08fdb709c191a5cf109b5b353518999
Roland4444 <turbopollux@rambler.ru>
2021-05-28 10:13:41 +0400

    adding DSL complete

--- src/main/kotlin/DSLGuided/requestsx/DSL.kt
+++ src/main/kotlin/DSLGuided/requestsx/DSL.kt
@@ -5,34 +5,17 @@ import DSLGuided.requestsx.PSA.PSADSLProcessor
 import DSLGuided.requestsx.PSA.PSASearchProcessor
 import DSLGuided.requestsx.SMS.SMSDSLProcessor
 import DSLGuided.requestsx.WProcessor.WProcessor
-import Message.abstractions.BinaryMessage
-import se.roland.abstractions.RolesStorage
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 import java.io.IOException
 import java.util.*
-
-
 class DSL {
-    var dslfile: String? = null
-
     constructor() {
         prepareMap()
         prepareDSLProcessors()
+        initDSL()
     }
-
-    constructor(dslfile_: String?) {
-        if (File(dslfile_).exists()) rs =
-            BinaryMessage.restored(BinaryMessage.readBytes(dslfile_)) as RolesStorage else {
-            rs = RolesStorage()
-            BinaryMessage.write(BinaryMessage.savedToBLOB(rs), dslfile_)
-        }
-        dslfile = dslfile_
-        prepareDSLProcessors()
-        prepareMap()
-    }
-
     var RequestsDSLProcessor: RequestsDSLProcessor? = null
     var PSADSLProcessor: PSADSLProcessor? = null
     var SMSDSLProcessor: SMSDSLProcessor? = null
@@ -41,7 +24,6 @@ class DSL {
     var dslProcessors: HashMap<String, DSLProcessor>? = null
     var urltoDSLProc: HashMap<String, String>? = null
     var WProcessor: WProcessor? = null
-    var rs: RolesStorage? = null
     fun prepareDSLProcessors() {
         RequestsDSLProcessor = RequestsDSLProcessor()
         SMSDSLProcessor = SMSDSLProcessor()
@@ -56,6 +38,16 @@ class DSL {
         dslProcessors!!["psaconnector"] = PSAConnector!!
         dslProcessors!!["psasearch"] = PSASearchProcessor!!
         dslProcessors!!["wprocessor"] = WProcessor!!
+    }
+
+    @Throws(IOException::class)
+    fun initDSL() {
+        dslProcessors?.get("psaconnector")?.render(getDSLforObject("psaconnector", "server"))
+        val psearch = dslProcessors?.get("psasearch") as PSASearchProcessor
+        val pconnector = dslProcessors!!.get("psaconnector") as PSAConnector
+        psearch.executor = pconnector.executor
+        PSADSLProcessor!!.executor  = pconnector.executor
+        PSADSLProcessor!!.psearch  = PSASearchProcessor!!
     }
 
     fun prepareMap() {
